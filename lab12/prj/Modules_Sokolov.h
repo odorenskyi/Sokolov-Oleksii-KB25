11f56aaf50eaa2b3861f9176bc05a14083d15541
Olexii-Sokolov <olexiisokolovgithub@gmail.com>
2026-05-26 16:24:48 +0300

    Перероблено ТС та виконано пункти 5-8

--- lab12/prj/Modules_Sokolov.h
+++ lab12/prj/Modules_Sokolov.h
@@ -6,27 +6,24 @@ private:
     double edge;
 
 public:
-    ClassLab12_Sokolov(double a = 1.0) {
-        if (!setEdge(a)) {
-            edge = 1.0;
-        }
-    }
-
-    bool setEdge(double a) {
-        if (a > 0.0) {
-            edge = a;
-            return true;
-        }
-        return false;
-    }
-
-    double getEdge() const {
-        return edge;
-    }
-
-    double getVolume() const {
-        return edge * edge * edge;
-    }
+    ClassLab12_Sokolov(double a = 1.0);
+    bool setEdge(double a);
+    double getEdge() const { return edge; }
+    double getVolume() const { return edge * edge * edge; }
 };
+
+ClassLab12_Sokolov::ClassLab12_Sokolov(double a) {
+    if (!setEdge(a)) {
+        edge = 1.0;
+    }
+}
+
+bool ClassLab12_Sokolov::setEdge(double a) {
+    if (a > 0.0) {
+        edge = a;
+        return true;
+    }
+    return false;
+}
 
 #endif // MODULESSOKOLOV_H
